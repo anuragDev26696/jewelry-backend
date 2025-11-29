ac8d3367feb5a08ebb7c5086ad168149c28eca19
Anurag-Kumar-Verma <anuragkumar26696@gmail.com>
2025-11-29 08:08:04 +0530

    using docker file

--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -279,7 +279,6 @@ export class BillsService {
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
         "--disable-gpu",
         "--no-zygote",
         "--single-process",
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-// import * as express from 'express';
-// import cors from 'cors';
+import * as express from 'express';
+import cors from 'cors';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
@@ -12,14 +12,9 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 4000;
   app.setGlobalPrefix('api');
-  app.useBodyParser('json', { limit: '10mb' });
-  app.useBodyParser('urlencoded', { extended: true, limit: '10mb' });
-  // app.use(cors());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+  app.use(cors())
   await app.listen(port);
   console.log(`Server running on http://localhost:${port}/api`);
 }
