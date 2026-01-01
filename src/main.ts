8514332423dfc033c2b34947c89818a03a8b615d
anuragDev26696 <anuragkumar26696@gmail.com>
2026-01-01 19:07:33 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 4000;
